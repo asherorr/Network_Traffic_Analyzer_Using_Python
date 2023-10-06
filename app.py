69dbf3421edb34b285af5ff3a4ce1a77533bc893
asherorr <asher@mailfence.com>
2023-10-05 21:54:35 -0400

    This commit will update the requirements.txt file and add pyshark to the list of imports in app.py.

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import pandas as pd
 import matplotlib as plt
-import networknx as nx
-
-wireshark_url = "C:\Users\asher\Downloads\tfp_capture.pcapng"
-wireshark_data = pd.read_csv(wireshark_url)
+import networkx as nx
+import pyshark
