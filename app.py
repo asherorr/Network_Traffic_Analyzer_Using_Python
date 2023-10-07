b2755651f50ca0aa0a835de96c5105e372c35d63
asherorr <asher@mailfence.com>
2023-10-07 15:14:09 -0400

    This commit will add the analyze_network_traffic_with_pandas function.

--- app.py
+++ app.py
@@ -5,9 +5,23 @@ import pyshark
 
 
 def pyshark_analyzing_tool():
-    cap = pyshark.FileCapture(r"", display_filter='')
+    cap = pyshark.FileCapture(r'', display_filter='')
     for pkt in cap:
         print(pkt)
     
 
+def analyze_network_traffic_with_pandas():
+    cap = pyshark.FileCapture(r'', display_filter='')
+    data = []
+    
+    for packet in cap:
+        data.append({
+            'source': packet.ip.src,
+            'destination': packet.ip.dst,
+            'protocol': packet.transport_layer,
+            'length': packet.length
+        })
 
+    df = pd.DataFrame(data)
+    print(df.head())
+    print(df.groupby('protocol')['length'].sum())
