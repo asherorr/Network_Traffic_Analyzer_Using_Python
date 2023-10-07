c0fdc07322c52a6cdd9d04390bce2bb82dcebe73
asherorr <asher@mailfence.com>
2023-10-07 15:07:23 -0400

    This commit will add the skeleton of the pyshark_analyzing_tool function.

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ import pandas as pd
 import matplotlib as plt
 import networkx as nx
 import pyshark
+
+
+def pyshark_analyzing_tool():
+    cap = pyshark.FileCapture(r"C:\Users\asher\Downloads\tfp_capture.pcapng", display_filter='')
+    for pkt in cap:
+        print(pkt)
+    
+
+
