9717cf5bbc22e867426dc6df350fd9664bc5e4c3
asherorr <asher@mailfence.com>
2023-10-13 15:03:36 -0400

    This commit will add a function to scan the local machine for open ports and add the pretty_print module to the pyshark_pcap_file_reader function.

--- app.py
+++ app.py
@@ -2,16 +2,33 @@ import pandas as pd
 import matplotlib as plt
 import networkx as nx
 import pyshark
+import socket
 
 
-def pyshark_analyzing_tool():
+def scan_open_ports_on_lm():
+    target = 'localhost'
+    port = 22
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(5)
+
+    result = s.connect_ex((target, port))
+
+    if result == 0:
+        print(f"Port {port} is open")
+    else:
+        print(f"Port {port} is closed")
+
+
+
+def pyshark_pcap_file_reader():
     cap = pyshark.FileCapture(r'', display_filter='')
     for pkt in cap:
-        print(pkt)
+        print(pkt.pretty_print())
     
 
 def analyze_network_traffic_with_pandas():
-    cap = pyshark.FileCapture(r'', display_filter='')
+    cap = pyshark.FileCapture(r'C:\Users\asher\Downloads\tfp_capture.pcapng', display_filter='tcp')
     data = []
     
     for packet in cap:
@@ -25,3 +42,4 @@ def analyze_network_traffic_with_pandas():
     df = pd.DataFrame(data)
     print(df.head())
     print(df.groupby('protocol')['length'].sum())
+
