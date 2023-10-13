a80b906262027e6c8285742c4485ebe947c026bd
asherorr <asher@mailfence.com>
2023-10-13 15:04:54 -0400

    This commit will update the app.py file to abide by PEP-8 style guidelines.

--- app.py
+++ app.py
@@ -8,17 +8,13 @@ import socket
 def scan_open_ports_on_lm():
     target = 'localhost'
     port = 22
-
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(5)
-
     result = s.connect_ex((target, port))
-
     if result == 0:
         print(f"Port {port} is open")
     else:
         print(f"Port {port} is closed")
-
 
 
 def pyshark_pcap_file_reader():
@@ -28,9 +24,8 @@ def pyshark_pcap_file_reader():
     
 
 def analyze_network_traffic_with_pandas():
-    cap = pyshark.FileCapture(r'C:\Users\asher\Downloads\tfp_capture.pcapng', display_filter='tcp')
+    cap = pyshark.FileCapture(r'', display_filter='tcp')
     data = []
-    
     for packet in cap:
         data.append({
             'source': packet.ip.src,
@@ -38,7 +33,6 @@ def analyze_network_traffic_with_pandas():
             'protocol': packet.transport_layer,
             'length': packet.length
         })
-
     df = pd.DataFrame(data)
     print(df.head())
     print(df.groupby('protocol')['length'].sum())
